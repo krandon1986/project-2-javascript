44817606c8dc7e86c9b1b7b4cff052bdb416401a
krandon1986 <krandon1986@yahoo.co.uk>
2023-02-08 20:54:42 +0000

    Declare the constant for DOM elements and possible choices

---
+++ assets/js/game.js
@@ -0,0 +1,11 @@
+/**
+ * Declare constants for DOM elements
+ * and possible choice
+ */
+const buttons = document.getElementsByClassName("btn");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const message = document.getElementById("game-stats");
+const choices = ["rock", "paper", "scissor"];
