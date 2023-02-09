b803e3d5cff9e5dcf1d98baa61337964d4ccb1f5
krandon1986 <krandon1986@yahoo.co.uk>
2023-02-09 16:36:31 +0000

    Make some change to the javascript to correct some errors

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,18 @@
  * and possible choice
  */
 const buttons = document.getElementsByClassName("btn");
-const playerScore = document.getElementById("player-score");
-const computerScore = document.getElementById("computer-score");
+const playerScore = document.getElementById("playerscore");
+const computerScore = document.getElementById("computerscore");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const message = document.getElementById("game-stats");
-const choices = ["rock", "paper", "scissor"]; 
+const rockIcon = "rock";
+const paperIcon = "paper";
+const scissorIcon = "scissor";
+const choices = [rockIcon, paperIcon, scissorIcon]; 
 const playerIcon = document.querySelectorAll(".show-player");
 const computerIcon = document.querySelectorAll(".show-computer");
+
 
 /**
  * Add the event listener to all the three buttons
@@ -38,61 +42,65 @@ function playGame(playerChoice) {
     computerImage.alt = choices[computerChoice];
 
     let result = checkwinner(choices[computerChoice], choices[playerChoice]);
-
+    
     updateScore(result);
 }
 
 /**
  * Displaying the victory of either the player or the computer
  */
-
+let playerscore = 1;
+let computerscore = 1;
 
 const tie = ()=>{
     message.innerHTML = "Tie !";
-    message.style.color = "orange";
+    message.style.color = 'orange';
 }
 
 const win = ()=>{
     message.innerHTML = "Player Won !";
-    message.style.color = "green";
+    message.style.color = 'green';
 }
 
 const lose = ()=>{
     message.innerHTML = "Computer Won !";
-    message.style.color = "red";
+    message.style.color = 'red';
 }
 
 
-/* If it is a tie */
- if(playerIcon.className === computerIcon.className){
+/*Scoring */
+function checkwinner(){
+    /* If it is a tie */
+    if(playerIcon.className === computerIcon.className){
     playerScore.innerHTML = playerScore.innerHTML;
     computerScore.innerHTML = computerScore.innerHTML;
     tie();
- }
+    }
 
- /* If it is not a tie */
- else if(playerIcon.className === "rock" && computerIcon.className === "scissor"){
-    playerScore.innerHTML = playerScore;
+    /* If it is not a tie */
+    else if(playerIcon.className === rockIcon && computerIcon.className === scissorIcon){
+    playerScore.innerHTML = playerscore;
     playerScore++;
     win();
- }else if(playerIcon.className === "rock" && computerIcon.className === "paper"){
-    computerScore.innerHTML = computerScore;
-    computerScore++;
+    }else if(playerIcon.className === rockIcon && computerIcon.className === paperIcon){
+    computerScore.innerHTML = computerscore;
+    computerscore++;
     lose();
- }else if(playerIcon.className === "paper" && computerIcon.className === "scissor"){
-    computerScore.innerHTML = computerScore;
-    computerScore++;
+    }else if(playerIcon.className === paperIcon && computerIcon.className === scissorIcon){
+    computerScore.innerHTML = computerscore;
+    computerscore++;
     lose();
- }else if(playerIcon.className === "paper" && computerIcon.className === "rock"){
-    playerScore.innerHTML = playerScore;
-    playerScore++;
+    }else if(playerIcon.className === paperIcon && computerIcon.className === rockIcon){
+    playerScore.innerHTML = playerscore;
+    playerscore++;
     win();
- }else if(playerIcon.className === "scissor" && computerIcon.className === "rock"){
-    computerScore.innerHTML = computerScore;
-    computerScore++;
+    }else if(playerIcon.className === scissorIcon && computerIcon.className === rockIcon){
+    computerScore.innerHTML = computerscore;
+    computerscore++;
     lose();
- }else if(playerIcon.className === "scissor" && computerIcon.className === "paper"){
-    playerScore.innerHTML = playerScore;
-    playerScore++;
+    }else if(playerIcon.className === scissorIcon && computerIcon.className === paperIcon){
+    playerScore.innerHTML = playerscore;
+    playerscore++;
     win();
- }
+    }
+}
